f0541d9dcac3e39fe6b0f90ae263760aa6ddee87
BuildTools <unconfigured@null.spigotmc.org>
2023-05-26 20:18:14 +0200

    Test

---
+++ src/main/java/com/SteamTOTPGenerator/SteamTotpGeneratorApplication.java
@@ -0,0 +1,14 @@
+package com.SteamTOTPGenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SteamTotpGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SteamTotpGeneratorApplication.class, args);
+		System.out.println("Test");
+	}
+
+}
---
+++ src/test/java/com/SteamTOTPGenerator/SteamTotpGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SteamTOTPGenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SteamTotpGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
